b0cf08694c8a7d216276e89ed0a7c8a79252ccee
Bjørnar Alvestad <BjAlvestad@gmail.com>
2024-07-27 10:35:33 +0000

    WIP Add parsing of stuct declaration in FB

--- simatic-scl-lsp/simatic-scl-lsp/test/parsing/parsing.test.ts
+++ simatic-scl-lsp/simatic-scl-lsp/test/parsing/parsing.test.ts
@@ -77,6 +77,60 @@ describe('Parsing tests', () => {
         `);
         //TODO: Find out why the variables below original get en extra intendation in expected. Should probably have been alligned.
     });
+
+    test('parse struct in FB', async () => {
+        document = await parse(`
+            FUNCTION_BLOCK "FB_MyFunctionBlock"
+            { S7_Optimized_Access := 'TRUE' }
+            AUTHOR : Someone
+            FAMILY : SomeFamily
+            VERSION : 0.1
+
+            VAR 
+                myInt : DINT;
+                myStruct : STRUCT
+                    myIntenal1 : DINT;
+                    myIntenal2 : BOOL;
+                END_STRUCT;
+            END_VAR
+
+            BEGIN
+                #myVar1 := 11;
+                #myVar2 := 22;
+                #internal2 := 22;
+            END_FUNCTION
+        `);
+
+        // check for absensce of parser errors the classic way:
+        //  deacivated, find a much more human readable way below!
+        // expect(document.parseResult.parserErrors).toHaveLength(0);
+
+        expect(
+            // here we use a (tagged) template expression to create a human readable representation
+            //  of the AST part we are interested in and that is to be compared to our expectation;
+            // prior to the tagged template expression we check for validity of the parsed document object
+            //  by means of the reusable function 'checkDocumentValid()' to sort out (critical) typos first;
+            checkDocumentValid(document) || s`
+                Top level declarations:
+                  ${document.parseResult.value?.vars?.map(p => p.name)?.join('\n  ')}
+                Inside myStruct:
+                  ${document.parseResult.value?.vars?.map(p => p.name)?.join('\n  ')}
+                Var usages in assignments:
+                  ${document.parseResult.value?.assignment?.map(g => g.var.$refText)?.join('\n  ')}
+            `
+        ).toBe(s`
+            Top level declarations::
+              myInt
+                myStruct
+            Inside myStruct:
+                myIntenal1
+                  myIntenal2
+            Var usages in assignments:
+              myVar1
+                myVar2
+                internal2
+        `);
+    });
 });
 
 function checkDocumentValid(document: LangiumDocument): string | undefined {
