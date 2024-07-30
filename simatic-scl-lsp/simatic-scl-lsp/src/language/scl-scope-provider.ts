4715cdf86145e0d27c4f10269a14e4f9fe14fa64
Bjørnar Alvestad <bjalvestad@gmail.com>
2024-07-30 12:18:58 +0200

    WIP: Scope experimentation - returns scope with every variable

--- simatic-scl-lsp/simatic-scl-lsp/src/language/scl-scope-provider.ts
+++ simatic-scl-lsp/simatic-scl-lsp/src/language/scl-scope-provider.ts
@@ -1,12 +1,42 @@
-import type { Scope } from 'langium';
-import { EMPTY_SCOPE } from 'langium';
+import type { ReferenceInfo, Scope } from 'langium';
+// import { EMPTY_SCOPE } from 'langium';
 import { DefaultScopeProvider } from 'langium';
+import { isMemberCall } from './generated/ast.js';
 
 /**
  * Scope provider that restricts scope to a single file
  */
 export class SclScopeProvider extends DefaultScopeProvider {
-    protected override getGlobalScope(referenceType: string): Scope {
-        return EMPTY_SCOPE;
+    // protected override getGlobalScope(referenceType: string): Scope {
+    //     return EMPTY_SCOPE;
+    // }
+
+    override getScope(context: ReferenceInfo): Scope {
+        console.log("****  INSIDE getScope() !!!  ****")
+        if (context.property === 'element' && isMemberCall(context.container)) {
+            console.log("  ** inside IF  **")
+            // const memberCall = context.container;
+            // const previous = memberCall.previous;
+            // console.log("    memberCurrent: " + memberCall)
+            // console.log("    memberCurrent type: " + memberCall.$type)
+            // console.log("    previous: " + previous)
+            // console.log("    previous type: " + previous?.$type)
+            // console.log(previous)
+            // if (!previous) {
+            //     console.log("      inside !previous")
+            //     return super.getScope(context)
+            // }
+        }
+
+        // When the target of our member call isn't a class
+        // This means it is either a primitive type or a type resolution error
+        // Simply return an empty scope
+        // return EMPTY_SCOPE;
+        return super.getScope(context);
     }
+
+    // private scopeClassMembers(classItem: Class): Scope {
+    //     const allMembers = getClassChain(classItem).flatMap(e => e.members);
+    //     return this.createScopeForNodes(allMembers);
+    // }
 }
---
+++ simatic-scl-lsp/simatic-scl-lsp/test/parsing/debug-scope.test.ts
@@ -0,0 +1,59 @@
+import { beforeAll, describe, expect, test } from "vitest";
+import { EmptyFileSystem, type LangiumDocument } from "langium";
+import { expandToString as s } from "langium/generate";
+import { parseHelper } from "langium/test";
+import { createSclServices } from "../../src/language/scl-module.js";
+import { Model, isModel } from "../../src/language/generated/ast.js";
+
+let services: ReturnType<typeof createSclServices>;
+let parse:    ReturnType<typeof parseHelper<Model>>;
+let document: LangiumDocument<Model> | undefined;
+
+beforeAll(async () => {
+    services = createSclServices(EmptyFileSystem);
+    parse = parseHelper<Model>(services.Scl);
+
+    // activate the following if your linking test requires elements from a built-in library, for example
+    // await services.shared.workspace.WorkspaceManager.initializeWorkspace([]);
+});
+
+describe('Print to log for debug tests', () => {
+
+    test('parse simple model', async () => {
+        document = await parse(`
+            FUNCTION_BLOCK "FB_MyFunctionBlock"
+            { S7_Optimized_Access := 'TRUE' }
+            AUTHOR : Someone
+            FAMILY : SomeFamily
+            VERSION : 0.1
+
+            VAR 
+              myStruct: STRUCT
+                internal1 : DINT;
+                internal2 : DINT;
+              END_STRUCT;
+            END_VAR
+
+            VAR_TEMP 
+                myVar1 : DINT;   // Comment for my variable 1
+                myVar2 : DINT;   // Comment for my variable 2
+            END_VAR
+
+            BEGIN
+                #myVar1 := 11;
+                #myVar2 := 22;
+                #myStruct.internal2 := 22;
+            END_FUNCTION
+        `);
+    });
+});
+  
+function checkDocumentValid(document: LangiumDocument): string | undefined {
+    return document.parseResult.parserErrors.length && s`
+        Parser errors:
+          ${document.parseResult.parserErrors.map(e => e.message).join('\n  ')}
+    `
+        || document.parseResult.value === undefined && `ParseResult is 'undefined'.`
+        || !isModel(document.parseResult.value) && `Root AST object is a ${document.parseResult.value.$type}, expected a '${Model}'.`
+        || undefined;
+}
