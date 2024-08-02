6018a9e447ad614b2dfa32afea965b2b1f5d6a24
Bjørnar Alvestad <bjalvestad@gmail.com>
2024-08-02 16:15:14 +0200

    Add hover provider

---
+++ simatic-scl-lsp/simatic-scl-lsp/src/language/lsp/scl-hover-provider.ts
@@ -0,0 +1,52 @@
+import { AstNode } from "langium";
+import { Hover } from "vscode-languageclient";
+import { isStruct, isNamedElement } from "../generated/ast.js";
+import { isErrorType, typeToString } from "../type-system/descriptions.js";
+import { inferType } from "../type-system/infer.js";
+import { AstNodeHoverProvider } from "langium/lsp";
+
+export class SclHoverProvider extends AstNodeHoverProvider {
+    protected getAstNodeHoverContent(node: AstNode): Hover | undefined {
+        if (isStruct(node)) {
+            return {
+                contents: {
+                    kind: 'markdown',
+                    language: 'scl',
+                    value: `struct ${node.vars}}`
+                }
+            }
+        } else if (isNamedElement(node)) {
+            const type = inferType(node, new Map());
+            if (isErrorType(type)) {
+                return undefined;
+            }
+            if (node.type.primitive) {
+                return {
+                    contents: {
+                        kind: 'markdown',
+                        language: 'scl',
+                        value: `var ${node.name}: ${node.type.primitive} (Inferred type is ${typeToString(type)})`
+                    }
+                }
+            } else if (node.type.struct) {
+                return {
+                    contents: {
+                        kind: 'markdown',
+                        language: 'scl',
+                        value: `var ${node.name}: STRUCT ${node.type.struct.vars.map(v => `\n  ${v.name} : ${v.type.primitive ?? 'STRUCT'}`)}\n(Inferred type is ${typeToString(type)})`
+                    }
+                }
+            } else if (node.type.UdtRef) {
+                // This hover does not trigger for some reason.
+                return {
+                    contents: {
+                        kind: 'markdown',
+                        language: 'scl',
+                        value: `var ${node.name}: UDT ${node.type.UdtRef} (Inferred type is ${typeToString(type)})`
+                    }
+                }
+            }
+        }
+        return undefined;
+    }
+}
