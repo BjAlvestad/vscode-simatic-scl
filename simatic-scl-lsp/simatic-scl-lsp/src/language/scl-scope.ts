b17ad552c9761f75ba0bc0e0ef037a0a59a298cb
Bjørnar Alvestad <bjalvestad@gmail.com>
2024-08-02 08:01:38 +0200

    Comment out code causing infinite loop

--- simatic-scl-lsp/simatic-scl-lsp/src/language/scl-scope.ts
+++ simatic-scl-lsp/simatic-scl-lsp/src/language/scl-scope.ts
@@ -1,7 +1,7 @@
 import type { ReferenceInfo, Scope } from 'langium';
 import { EMPTY_SCOPE } from 'langium';
 import { DefaultScopeProvider } from 'langium';
-import { isMemberCall, MemberCall, VariableDeclaration } from './generated/ast.js';
+import { isMemberCall, MemberCall } from './generated/ast.js';
 // import { inferType } from './type-system/infer.js';
 
 /**
@@ -31,17 +31,17 @@ export class SclScopeProvider extends DefaultScopeProvider {
                 console.log(localVar.$type)  // ==> Local variable
                 console.log(localVar.element?.$refText)  // ==> myStruct (when auto completing with dot behind myStruct)
                 
-                const varDec = (localVar.element?.ref as VariableDeclaration)
-                console.log(varDec.name)
-                const memberCallIsStruct = varDec.type.struct != undefined;
-                console.log(memberCallIsStruct ? "IS STRUCT" : "Is not struct")
-                if (varDec.type.struct) {
-                    console.log(varDec.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`) ?? "IS STRUCT")  // Prints internal structure of `myStruct` when putting `.` behind it
-                    const varsInStruct = varDec.type.struct?.vars
-                    return super.createScopeForNodes(varsInStruct)  // This actually returns list of elements in struct. But it gives "Error resolving reference on it afterwards"
-                } else {
-                    console.log("WAS NOT STRUCT SO DO NOT RETURN STRUCT SCOPE")
-                }
+                // const varDec = (localVar.element?.ref as VariableDeclaration)
+                // console.log(varDec.name)
+                // const memberCallIsStruct = varDec.type.struct != undefined;
+                // console.log(memberCallIsStruct ? "IS STRUCT" : "Is not struct")
+                // if (varDec.type.struct) {
+                //     console.log(varDec.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`) ?? "IS STRUCT")  // Prints internal structure of `myStruct` when putting `.` behind it
+                //     const varsInStruct = varDec.type.struct?.vars
+                //     return super.createScopeForNodes(varsInStruct)  // This actually returns list of elements in struct. But it gives "Error resolving reference on it afterwards"
+                // } else {
+                //     console.log("WAS NOT STRUCT SO DO NOT RETURN STRUCT SCOPE")
+                // }
             } else {
                 console.log("Was not memberCall...")
             }
@@ -55,34 +55,34 @@ export class SclScopeProvider extends DefaultScopeProvider {
             /** CURRENTLY HAS NO EFFECT - Trying to get it to complete on nested structs.
              * It enters the function, but I cannot find the elements inside the struct to return
              */
-            // const varDec = (memberCall?.$container as VariableDeclaration)
-            if (isMemberCall(previous)) {
-                console.log("Previous isMemberCall")
-                const prevCall = previous as MemberCall
-                const struct = prevCall.element?.ref?.type.struct;
-                if (struct) {
-                    console.log("!!!! IT IS STRUCT !!!!!")
-                    this.logTypeInfo(memberCall);
-                    console.log("         Vars inside previous   " + (prevCall.element?.ref?.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`)))
-                    console.log("         Vars inside current   " + (memberCall.element?.ref?.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`)))
-                    // console.log("         Vars inside current element   " + (memberCall.element?.ref?.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`)))
+            // // const varDec = (memberCall?.$container as VariableDeclaration)
+            // if (isMemberCall(previous)) {
+            //     console.log("Previous isMemberCall")
+            //     const prevCall = previous as MemberCall
+            //     const struct = prevCall.element?.ref?.type.struct;
+            //     if (struct) {
+            //         console.log("!!!! IT IS STRUCT !!!!!")
+            //         this.logTypeInfo(memberCall);
+            //         console.log("         Vars inside previous   " + (prevCall.element?.ref?.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`)))
+            //         console.log("         Vars inside current   " + (memberCall.element?.ref?.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`)))
+            //         // console.log("         Vars inside current element   " + (memberCall.element?.ref?.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`)))
 
-                    const currentVarDec = memberCall.element as unknown as VariableDeclaration
-                    console.log(`         Is currentVarDec undefined? ${(currentVarDec === undefined)}`)
-                    console.log(`         Has currentVarDec type? ${(currentVarDec.type)}`)
-                    console.log(`         Has currentVarDec struct? ${(currentVarDec.type.struct)}`)
-                    console.log(`         Vars inside element  ${(currentVarDec.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`))}`)
+            //         const currentVarDec = memberCall.element as unknown as VariableDeclaration
+            //         console.log(`         Is currentVarDec undefined? ${(currentVarDec === undefined)}`)
+            //         console.log(`         Has currentVarDec type? ${(currentVarDec.type)}`)
+            //         console.log(`         Has currentVarDec struct? ${(currentVarDec.type.struct)}`)
+            //         console.log(`         Vars inside element  ${(currentVarDec.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`))}`)
 
-                    // console.log("         Vars inside current element   " + (memberCall.element?.ref?.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`)))
-                    // console.log("         Vars inside previous element   " + (prevCall.element?.ref?.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`)))
+            //         // console.log("         Vars inside current element   " + (memberCall.element?.ref?.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`)))
+            //         // console.log("         Vars inside previous element   " + (prevCall.element?.ref?.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`)))
                     
-                    if (memberCall.element?.ref?.type.struct) {
-                        console.log("no loop")
-                        console.log(`         MemberCall element ref is struct type? ${memberCall.element?.ref?.type.struct}`)  // Call to memberCall.element?.ref Causes infinite loop
-                    }
-                    return super.createScopeForNodes(struct.vars)
-                }
-            }
+            //         if (memberCall.element?.ref?.type.struct) {
+            //             console.log("no loop")
+            //             console.log(`         MemberCall element ref is struct type? ${memberCall.element?.ref?.type.struct}`)  // Call to memberCall.element?.ref Causes infinite loop
+            //         }
+            //         return super.createScopeForNodes(struct.vars)
+            //     }
+            // }
         } else {
             console.log("Skipping 'if' with context.property " + context.property)
         }
@@ -117,24 +117,24 @@ export class SclScopeProvider extends DefaultScopeProvider {
         } else {
             console.log("    'previous' does not exist.");
         }
-        if (isMemberCall(previous) && previous.previous) {
-            console.log("    '2x previous' type: " + previous.previous.$type);
-            console.log("    '2x previous' element var refText: " + (isMemberCall(previous.previous)
-                ? `${(previous.previous as MemberCall).element?.ref?.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`)}`
-                : "Skipped. 'previous.previous' exists, but is not MemberCall."
-            ));
-            if (isMemberCall(previous.previous) && previous.previous.previous) {
-                console.log("    '3x previous' type: " + memberCall.$type);
-                console.log("    '3x previous' element var refText: " + (isMemberCall(previous.previous.previous)
-                    ? `${(previous.previous.previous as MemberCall).element?.ref?.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`)}`
-                    : "Skipped. 'previous.previous.previous' exists, but is not MemberCall."
-                ));
-            } else {
-                console.log("    'previous.previous.previous' does not exist.");
-            }
-        } else {
-            console.log("    'previous.previous' does not exist.");
-        }
+        // if (isMemberCall(previous) && previous.previous) {
+        //     console.log("    '2x previous' type: " + previous.previous.$type);
+        //     console.log("    '2x previous' element var refText: " + (isMemberCall(previous.previous)
+        //         ? `${(previous.previous as MemberCall).element?.ref?.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`)}`
+        //         : "Skipped. 'previous.previous' exists, but is not MemberCall."
+        //     ));
+        //     if (isMemberCall(previous.previous) && previous.previous.previous) {
+        //         console.log("    '3x previous' type: " + memberCall.$type);
+        //         console.log("    '3x previous' element var refText: " + (isMemberCall(previous.previous.previous)
+        //             ? `${(previous.previous.previous as MemberCall).element?.ref?.type.struct?.vars.map(g => `\n  ${g.name} : ${g.type.primitive ?? "not primitive"}`)}`
+        //             : "Skipped. 'previous.previous.previous' exists, but is not MemberCall."
+        //         ));
+        //     } else {
+        //         console.log("    'previous.previous.previous' does not exist.");
+        //     }
+        // } else {
+        //     console.log("    'previous.previous' does not exist.");
+        // }
 
         console.log("---\n");
     }
