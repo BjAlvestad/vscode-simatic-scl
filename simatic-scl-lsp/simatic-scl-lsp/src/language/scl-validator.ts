145ebf1f1c9e95865e8c4d92addcc564714e5810
Bjørnar Alvestad <BjAlvestad@gmail.com>
2024-08-10 09:09:52 +0200

    Remove/disable unused Person/Greeting grammar
    Had originally been left in since I wanted to keep validator as an example.
    Delete grammar part now, and comment out broken part from validator.

--- simatic-scl-lsp/simatic-scl-lsp/src/language/scl-validator.ts
+++ simatic-scl-lsp/simatic-scl-lsp/src/language/scl-validator.ts
@@ -1,5 +1,5 @@
-import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { SclAstType, Person } from './generated/ast.js';
+import type { /*ValidationAcceptor,*/ ValidationChecks } from 'langium';
+import type { SclAstType } from './generated/ast.js';
 import type { SclServices } from './scl-module.js';
 
 /**
@@ -9,7 +9,7 @@ export function registerValidationChecks(services: SclServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.SclValidator;
     const checks: ValidationChecks<SclAstType> = {
-        Person: validator.checkPersonStartsWithCapital
+        // Person: validator.checkPersonStartsWithCapital
     };
     registry.register(checks, validator);
 }
@@ -19,13 +19,13 @@ export function registerValidationChecks(services: SclServices) {
  */
 export class SclValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
-    }
+    // checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+    //     // if (person.name) {
+    //     //     const firstChar = person.name.substring(0, 1);
+    //     //     if (firstChar.toUpperCase() !== firstChar) {
+    //     //         accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+    //     //     }
+    //     // }
+    // }
 
 }
