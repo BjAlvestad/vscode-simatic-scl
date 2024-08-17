9c1b681ff98d5a2af3a696a890cc4d8c03f86c7f
Bjørnar Alvestad <BjAlvestad@gmail.com>
2024-08-17 18:51:05 +0200

    Refactor built in convert functions to a more compact format

--- src/language/built-in-scl-libraries/built-in-scl-library-functions.ts
+++ src/language/built-in-scl-libraries/built-in-scl-library-functions.ts
@@ -1,18 +1,16 @@
-export const UDINT_TO_TIME = `
-FUNCTION UDINT_TO_TIME : Time
-VERSION : 0.1
+// Built in convert functions
+export const UDINT_TO_TIME = createConvertFunction('UDINT', 'TIME');
+export const REAL_TO_UDINT = createConvertFunction('REAL', 'UDINT');
 
-BEGIN
-END_FUNCTION
-`.trimStart();
+function createConvertFunction(from: string, to: string): string {
+    return `
+    FUNCTION ${from}_TO_${to} : ${to}
+    VERSION : 0.1
 
-export const REAL_TO_UDINT = `
-FUNCTION REAL_TO_UDINT : UDINT
-VERSION : 0.1
-
-BEGIN
-END_FUNCTION
-`.trimStart();
+    BEGIN
+    END_FUNCTION
+    `.trimStart();
+}
 
 // scl-workspace-manager.ts and scl-library-file-system-provider.ts use this map to register
 // the libraries as documents, and to provide correct document for navigation in VS Code.
