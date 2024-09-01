9d52228f46ac4e545f05489593e2518f9e4bc5b0
Bjørnar Alvestad <BjAlvestad@gmail.com>
2024-09-01 13:57:32 +0200

    WIP: Get relative path for files in `includeEntry`

--- src/language/scl-workspace-manager.ts
+++ src/language/scl-workspace-manager.ts
@@ -10,6 +10,7 @@ import { WorkspaceFolder } from 'vscode-languageserver-types';
 import { URI } from "vscode-uri";
 import * as builtinLibrary from './built-in-scl-libraries/built-in-scl-library-functions.js'
 import { LangiumSharedServices } from "langium/lsp";
+import { absoluteToRelativePath } from "./utils.js";
 
 export class SclWorkspaceManager extends DefaultWorkspaceManager {
 
@@ -18,6 +19,7 @@ export class SclWorkspaceManager extends DefaultWorkspaceManager {
     constructor(services: LangiumSharedServices) {
         super(services);
         this.documentFactory = services.workspace.LangiumDocumentFactory;
+        // services.workspace.FileSystemProvider.readFile() //TODO: Read json file - must convert "scl-lsp-filter.json" to 
     }
 
     protected override async loadAdditionalDocuments(
@@ -38,9 +40,15 @@ export class SclWorkspaceManager extends DefaultWorkspaceManager {
             return false;
         }
         if (entry.isDirectory) {
+            const relativePath = absoluteToRelativePath(_workspaceFolder.uri, entry.uri.path);
+            console.log('Dir path:\t' + relativePath+ ` (${name})`)
+
+            // return name === 'SimpleStruct';
             return name !== 'node_modules' && name !== 'out';
         } else if (entry.isFile) {
             const extname = UriUtils.extname(entry.uri);
+            const relativePath = absoluteToRelativePath(_workspaceFolder.uri, entry.uri.path);
+            console.log('File path:\t' + relativePath + ` (${name})`)
             return fileExtensions.includes(extname);
         }
         return false;
--- src/language/utils.ts
+++ src/language/utils.ts
@@ -3,3 +3,15 @@ import { Model } from "./generated/ast.js";
 export function GetAllVarDecsFromModel(model: Model) {
     return model.decBlocks.flatMap(v => v.varDecs);
 }
+
+/**
+ * Get relative path from end of rootPath specified as argumetn
+ * @param rootPath full or part of path (skipping on left side) up to point to be removed from full path
+ * @param fullPath the full path to convert path relative to root
+ * @param skipLeft number of characters to ignore in rootPath when matching towards fullPath. Default 12 to skip e.g. `file:///c%3A/`
+ */
+export function absoluteToRelativePath(rootPath: string, fullPath: string, skipLeft = 12): string {
+    rootPath = rootPath.substring(skipLeft);
+    const relativePathStart = fullPath.indexOf(rootPath + "/") + rootPath.length + 1;
+    return fullPath.substring(relativePathStart)
+}
