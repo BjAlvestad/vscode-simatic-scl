06b63fa349687f658adeb1c6b34e93abd714899b
Bjørnar Alvestad <BjAlvestad@gmail.com>
2024-09-07 08:24:29 +0200

    WIP: Add filter by folder

--- src/language/scl-workspace-manager.ts
+++ src/language/scl-workspace-manager.ts
@@ -1,21 +1,27 @@
 import {
     AstNode,
     DefaultWorkspaceManager,
+    FileSystemNode,
     LangiumDocument,
     LangiumDocumentFactory,
+    UriUtils,
 } from "langium";
-import { WorkspaceFolder } from 'vscode-languageserver';
+import { WorkspaceFolder } from 'vscode-languageserver-types';
 import { URI } from "vscode-uri";
 import * as builtinLibrary from './built-in-scl-libraries/built-in-scl-library-functions.js'
 import { LangiumSharedServices } from "langium/lsp";
+import { absoluteToRelativePath } from "./utils.js";
+import * as fs from 'fs';
 
 export class SclWorkspaceManager extends DefaultWorkspaceManager {
 
     private documentFactory: LangiumDocumentFactory;
+    private filterContent: Set<string>;
 
     constructor(services: LangiumSharedServices) {
         super(services);
         this.documentFactory = services.workspace.LangiumDocumentFactory;
+        this.filterContent = this.readFilterFile('scl-lsp-filter.txt');
     }
 
     protected override async loadAdditionalDocuments(
@@ -28,5 +34,39 @@ export class SclWorkspaceManager extends DefaultWorkspaceManager {
             const fullUri = 'builtin://' + key;
             collector(this.documentFactory.fromString(builtinLibrary.uriMap[key], URI.parse(fullUri)));
         }
+    }
+
+    protected override includeEntry(_workspaceFolder: WorkspaceFolder, entry: FileSystemNode, fileExtensions: string[]): boolean {
+        const name = UriUtils.basename(entry.uri);
+        if (name.startsWith('.')) {
+            return false;
+        }
+        if (entry.isDirectory) {
+            const relativePath = absoluteToRelativePath(_workspaceFolder.uri, entry.uri.path);
+            const toBeIncluded = this.filterContent.has(relativePath);
+            // console.log('Dir path:\t' + relativePath+ ` (${name}) - ${toBeIncluded ? 'Included' : 'Excluded'}`)
+            return toBeIncluded && name !== 'node_modules' && name !== 'out';
+        } else if (entry.isFile) {
+            const extname = UriUtils.extname(entry.uri);
+            const relativePath = absoluteToRelativePath(_workspaceFolder.uri, entry.uri.path);
+            const toBeIncluded = this.filterContent.has(relativePath);
+            console.log('File path:\t' + relativePath + ` (${name})`)
+            return fileExtensions.includes(extname) && toBeIncluded;
+        }
+        return false;
+    }
+
+    private readFilterFile(fileName: string): Set<string> {
+        try {
+            const fileContent: string = fs.readFileSync(`C:\\Users\\BjAlv\\repos\\vscode-simatic-scl\\test\\examples\\${fileName}`, 'utf8');  //TODO: Find out how we can get workspace path
+            return new Set(
+                fileContent.split('\r?\n')
+                    .map(s => s.trim())
+                    .filter(s => s.length > 0 && !s.startsWith('#'))
+            );   
+        } catch (error) {
+            return new Set();
+        }
+        // console.log(this.filterContent);
     }
 }
