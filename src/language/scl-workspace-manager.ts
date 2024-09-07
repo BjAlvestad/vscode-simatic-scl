ca9ce8eece3a45440d1cea53d327809ddef2568a
Bjørnar Alvestad <BjAlvestad@gmail.com>
2024-09-07 16:07:00 +0200

    Can parse file, but due to aync, the content itself is not available outside

--- src/language/scl-workspace-manager.ts
+++ src/language/scl-workspace-manager.ts
@@ -1,31 +1,66 @@
 import {
     AstNode,
     DefaultWorkspaceManager,
+    FileSystemNode,
     LangiumDocument,
     LangiumDocumentFactory,
+    UriUtils,
+    WorkspaceFolder,
 } from "langium";
-import { WorkspaceFolder } from 'vscode-languageserver';
+// import { WorkspaceFolder } from 'vscode-languageserver';
 import { URI } from "vscode-uri";
 import * as builtinLibrary from './built-in-scl-libraries/built-in-scl-library-functions.js'
 import { LangiumSharedServices } from "langium/lsp";
+import { InitializeParams } from "vscode-languageclient";
+import fs from 'fs';
 
 export class SclWorkspaceManager extends DefaultWorkspaceManager {
 
     private documentFactory: LangiumDocumentFactory;
+    // private configUri?: URI;
 
     constructor(services: LangiumSharedServices) {
         super(services);
         this.documentFactory = services.workspace.LangiumDocumentFactory;
 
-        const workspaceFolder = `C:\\Users\\BjAlv\\repos\\vscode-simatic-scl\\test\\examples`
-        const fileName = 'config.lsp'
-        const path = `${workspaceFolder}\\${fileName}`
-        const pathUri = URI.file(path)
+        // const workspaceFolder = `C:\\Users\\BjAlv\\repos\\vscode-simatic-scl\\test\\examples`
+        // const fileName = 'config.lsp'
+        // const path = `${workspaceFolder}\\${fileName}`
+        // const pathUri = URI.file(path)
 
-        this.fileSystemProvider.readFile(pathUri).then(file => {
-            console.log(`file content: ${file}`)
-        })
+        // this.fileSystemProvider.readFile(pathUri).then(file => {
+        //     console.log(`file content: ${file}`)
+        // })
+
+            // context.connection?.workspace.getWorkspaceFolders().then(f => {
+    //     console.log(`workspace: ${f}`)
+    // })
     }
+
+    // override async initializeWorkspace(folders: WorkspaceFolder[], cancelToken = CancellationToken.None): Promise<void> {
+    //     await super.initializeWorkspace(folders, cancelToken);
+    //     // console.log(`folders from inside initializeWorkspace: ${folders[0].name} ${folders[0].uri}`)
+    // }
+
+    override initialize(params: InitializeParams): void {
+        console.log(params.workspaceFolders)
+        const configFileName = 'config.lsp'
+        if (params.workspaceFolders && params.workspaceFolders[0]) {
+            const workspaceRootPath = params.workspaceFolders[0].uri;
+            console.log(`workspaceRootPath: ${workspaceRootPath}`);
+            const configFilePath = `${workspaceRootPath}/${configFileName}`;
+            console.log(`config path: ${configFilePath}`);
+            const path = URI.file(params.workspaceFolders[0].uri)
+            console.log(`path: ${path.path}\n  fsPath: ${path.fsPath}`)
+            this.getFoldersToInclude()
+        }
+
+        super.initialize(params);
+    }
+
+    // override async initializeWorkspace(folders: WorkspaceFolder[], cancelToken?: CancellationToken): Promise<void> {
+    //     super.initializeWorkspace(folders, cancelToken);
+    // }
 
     protected override async loadAdditionalDocuments(
         folders: WorkspaceFolder[],
@@ -39,6 +74,11 @@ export class SclWorkspaceManager extends DefaultWorkspaceManager {
         }
     }
 
+    protected override async traverseFolder(workspaceFolder: WorkspaceFolder, folderPath: URI, fileExtensions: string[], collector: (document: LangiumDocument) => void): Promise<void> {
+        super.traverseFolder(workspaceFolder, folderPath, fileExtensions, collector);
+    }
+
+
     protected override includeEntry(_workspaceFolder: WorkspaceFolder, entry: FileSystemNode, fileExtensions: string[]): boolean {
         const name = UriUtils.basename(entry.uri);
         if (name.startsWith('.')) {
@@ -51,5 +91,28 @@ export class SclWorkspaceManager extends DefaultWorkspaceManager {
             return fileExtensions.includes(extname);
         }
         return false;
+    }
+
+    private getFoldersToInclude() {
+        const fileContent = this.getFile('config.lsp');
+        const includeFolders = fileContent.split('\r?\n');
+        console.log(`splitting '${fileContent}' into array of include folders:\n${includeFolders}`)
+        return includeFolders;
+    }
+
+    private getFile(configFileName: string) : string {
+        // const fs = require('fs');
+        let fileContent = '';
+        fs.readFile(configFileName, 'utf8', (err, data) => {
+            if (err) {
+                console.error('Error reading the config file: ' + err);
+                return;
+            }
+            console.log('Config file contents:', data);
+            fileContent = data;
+            return;
+        });
+        
+        return fileContent;
     }
 }
