cdb7e44870965637d2f65f3b3ba78bde546755b1
Bjørnar Alvestad <BjAlvestad@gmail.com>
2024-09-07 17:21:25 +0200

    Add includeFolder filter possibility
    Can read file in root of workspace that contains list of folders to be
    included. If file does not exist, all folders gets included. Note that
    that files directly in root of working directory will still get included
    regardless of filter config.
    
    The filter is only applied for the initial loading. If files that are not
    included in the filter gets opened in VS Code, that they will get loaded
    into the LSP by VS Code, even if they are not included in the filter list.

--- src/language/scl-workspace-manager.ts
+++ src/language/scl-workspace-manager.ts
@@ -1,21 +1,31 @@
 import {
     AstNode,
     DefaultWorkspaceManager,
+    FileSystemNode,
     LangiumDocument,
     LangiumDocumentFactory,
+    UriUtils,
 } from "langium";
 import { WorkspaceFolder } from 'vscode-languageserver';
 import { URI } from "vscode-uri";
 import * as builtinLibrary from './built-in-scl-libraries/built-in-scl-library-functions.js'
 import { LangiumSharedServices } from "langium/lsp";
+import { InitializeParams } from "vscode-languageclient";
+import fs from 'fs';
 
 export class SclWorkspaceManager extends DefaultWorkspaceManager {
 
     private documentFactory: LangiumDocumentFactory;
+    private includeFolders: string[] = [];
 
     constructor(services: LangiumSharedServices) {
         super(services);
         this.documentFactory = services.workspace.LangiumDocumentFactory;
+    }
+
+    override initialize(params: InitializeParams): void {
+        this.includeFolders = this.getFoldersToInclude('includeFoldersFilter.scl-lsp');
+        super.initialize(params);
     }
 
     protected override async loadAdditionalDocuments(
@@ -31,16 +41,43 @@ export class SclWorkspaceManager extends DefaultWorkspaceManager {
     }
 
     protected override includeEntry(_workspaceFolder: WorkspaceFolder, entry: FileSystemNode, fileExtensions: string[]): boolean {
+        // console.log(`Inside includeEntry: ${this.includeFolders.length} - [${this.includeFolders.join(', ')}]`);
+
         const name = UriUtils.basename(entry.uri);
         if (name.startsWith('.')) {
             return false;
         }
         if (entry.isDirectory) {
+            if ((this.includeFolders.length > 0)) {
+                return (
+                    name !== 'node_modules'
+                    && name !== 'out'
+                    && this.includeFolders.includes(name)
+                );
+            }
             return name !== 'node_modules' && name !== 'out';
         } else if (entry.isFile) {
             const extname = UriUtils.extname(entry.uri);
             return fileExtensions.includes(extname);
         }
         return false;
+    }
+
+    private getFoldersToInclude(configFileName: string) {
+        const fileContent = this.getFile(configFileName);
+        const includeFolders = fileContent.split('\n').map(s => s.trim()).filter(s => s.length > 0);
+        // console.log(`splitting string:\n'${fileContent}'\ninto array of include folders: [${includeFolders}]`)
+        return includeFolders;
+    }
+
+    private getFile(configFileName: string): string {
+        try {
+            let fileContent = fs.readFileSync(configFileName, 'utf8');
+            // console.log('Config file contents:', fileContent);
+            return fileContent;
+        } catch (err) {
+            console.log(`Could not read config file '${configFileName}' for folders to include`)
+            return '';
+        }
     }
 }
