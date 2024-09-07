883f8ec3c6dc9fd7edda444f804bfb39b3c83f32
Bjørnar Alvestad <BjAlvestad@gmail.com>
2024-09-07 17:06:03 +0200

    Clean up unused code in scl-workspace-manager

--- src/language/scl-workspace-manager.ts
+++ src/language/scl-workspace-manager.ts
@@ -17,43 +17,17 @@ import fs from 'fs';
 export class SclWorkspaceManager extends DefaultWorkspaceManager {
 
     private documentFactory: LangiumDocumentFactory;
-    // private configUri?: URI;
     private includeFolders: string[] = [];
 
     constructor(services: LangiumSharedServices) {
         super(services);
         this.documentFactory = services.workspace.LangiumDocumentFactory;
-
-        // const workspaceFolder = `C:\\Users\\BjAlv\\repos\\vscode-simatic-scl\\test\\examples`
-        // const fileName = 'config.lsp'
-        // const path = `${workspaceFolder}\\${fileName}`
-        // const pathUri = URI.file(path)
-
-        // this.fileSystemProvider.readFile(pathUri).then(file => {
-        //     console.log(`file content: ${file}`)
-        // })
-
-            // context.connection?.workspace.getWorkspaceFolders().then(f => {
-    //     console.log(`workspace: ${f}`)
-    // })
     }
-
-    // override async initializeWorkspace(folders: WorkspaceFolder[], cancelToken = CancellationToken.None): Promise<void> {
-    //     await super.initializeWorkspace(folders, cancelToken);
-    //     // console.log(`folders from inside initializeWorkspace: ${folders[0].name} ${folders[0].uri}`)
-    // }
 
     override initialize(params: InitializeParams): void {
-        console.log(params.workspaceFolders)
-        const configFileName = 'config.lsp'
         this.includeFolders = this.getFoldersToInclude();
-
         super.initialize(params);
     }
-
-    // override async initializeWorkspace(folders: WorkspaceFolder[], cancelToken?: CancellationToken): Promise<void> {
-    //     super.initializeWorkspace(folders, cancelToken);
-    // }
 
     protected override async loadAdditionalDocuments(
         folders: WorkspaceFolder[],
@@ -67,11 +41,6 @@ export class SclWorkspaceManager extends DefaultWorkspaceManager {
         }
     }
 
-    protected override async traverseFolder(workspaceFolder: WorkspaceFolder, folderPath: URI, fileExtensions: string[], collector: (document: LangiumDocument) => void): Promise<void> {
-        super.traverseFolder(workspaceFolder, folderPath, fileExtensions, collector);
-    }
-
-
     protected override includeEntry(_workspaceFolder: WorkspaceFolder, entry: FileSystemNode, fileExtensions: string[]): boolean {
         // console.log(`Inside includeEntry: ${this.includeFolders.length} - [${this.includeFolders.join(', ')}]`);
 
@@ -81,7 +50,11 @@ export class SclWorkspaceManager extends DefaultWorkspaceManager {
         }
         if (entry.isDirectory) {
             if ((this.includeFolders.length > 0)) {
-                return name !== 'node_modules' && name !== 'out' && this.includeFolders.includes(name);
+                return (
+                    name !== 'node_modules'
+                    && name !== 'out'
+                    && this.includeFolders.includes(name)
+                );
             }
             return name !== 'node_modules' && name !== 'out';
         } else if (entry.isFile) {
@@ -94,7 +67,7 @@ export class SclWorkspaceManager extends DefaultWorkspaceManager {
     private getFoldersToInclude() {
         const fileContent = this.getFile('config.lsp');
         const includeFolders = fileContent.split('\n').map(s => s.trim()).filter(s => s.length > 0);
-        console.log(`splitting string:\n'${fileContent}'\ninto array of include folders: [${includeFolders}]`)
+        // console.log(`splitting string:\n'${fileContent}'\ninto array of include folders: [${includeFolders}]`)
         return includeFolders;
     }
 
