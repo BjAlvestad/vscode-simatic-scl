62e0d8a72b21e6d8be43608f4cdeb2ed91e98ccd
Bjørnar Alvestad <BjAlvestad@gmail.com>
2024-09-08 10:56:14 +0200

    Match against absolute path and Include subfolders

--- src/language/scl-workspace-manager.ts
+++ src/language/scl-workspace-manager.ts
@@ -12,6 +12,7 @@ import * as builtinLibrary from './built-in-scl-libraries/built-in-scl-library-f
 import { LangiumSharedServices } from "langium/lsp";
 import { InitializeParams } from "vscode-languageclient";
 import fs from 'fs';
+import path from 'path';
 
 export class SclWorkspaceManager extends DefaultWorkspaceManager {
 
@@ -48,12 +49,18 @@ export class SclWorkspaceManager extends DefaultWorkspaceManager {
             return false;
         }
         if (entry.isDirectory) {
+            // console.log(`for ${name}, we have workspace folder ${_workspaceFolder.uri} and entry ${entry.uri.fsPath}`)
             if ((this.includeFolders.length > 0)) {
-                return (
+                const shouldInclude = (
                     name !== 'node_modules'
                     && name !== 'out'
-                    && this.includeFolders.includes(name)
+                    && (this.includeFolders.includes(entry.uri.fsPath) || this.isSubfolderToInclude(entry.uri.fsPath)) 
                 );
+                // console.info(`comparing ${entry.uri.fsPath} against filter. Include? ${shouldInclude}`)
+                if (shouldInclude) {
+                    console.info(`Loading ${entry.uri.fsPath} into workspace since it matches filter...`)
+                }
+                return shouldInclude;
             }
             return name !== 'node_modules' && name !== 'out';
         } else if (entry.isFile) {
@@ -64,13 +71,16 @@ export class SclWorkspaceManager extends DefaultWorkspaceManager {
     }
 
     private getFoldersToInclude(configFileName: string) {
-        const fileContent = this.getFile(configFileName);
-        const includeFolders = fileContent.split('\n').map(s => s.trim()).filter(s => s.length > 0);
-        // console.log(`splitting string:\n'${fileContent}'\ninto array of include folders: [${includeFolders}]`)
-        return includeFolders;
+        const absolutePath = path.resolve()
+        const fileContent = this.getFileFromFilesystem(configFileName);
+        const includeFoldersRelative = fileContent.split('\n').map(s => s.trim()).filter(s => s.length > 0);
+        // console.log(`splitting string:\n'${fileContent}'\ninto array of include folders: [${includeFoldersRelative}]`)
+        const includeFoldersAbsolute = includeFoldersRelative.map(p => path.join(absolutePath, p))
+        // console.log(`Combining :\n'${absolutePath}'\ninto array of include folders: [${includeFoldersAbsolute}]`)
+        return includeFoldersAbsolute;
     }
 
-    private getFile(configFileName: string): string {
+    private getFileFromFilesystem(configFileName: string): string {
         try {
             let fileContent = fs.readFileSync(configFileName, 'utf8');
             // console.log('Config file contents:', fileContent);
@@ -79,5 +89,14 @@ export class SclWorkspaceManager extends DefaultWorkspaceManager {
             console.log(`Could not read config file '${configFileName}' for folders to include`)
             return '';
         }
+    }
+
+    private isSubfolderToInclude(fsPath: string): boolean {
+        for (let path of this.includeFolders) {
+            if (fsPath.startsWith(path)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
